d01154de20ad003ba8c8f024343229af4ca75598
mikitora <miki.tora.0703@gmail.com>
2025-05-12 07:59:57 +0900

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
 function toggleMenu() {
-  const menu = document.getElementById('sideMenu');
-  menu.classList.toggle('open');
+  const menu = document.getElementById("menu");
+  menu.classList.toggle("open");
 }
