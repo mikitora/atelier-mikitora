2f172f69796126d635ee1e41a62aca9df897f722
mikitora <miki.tora.0703@gmail.com>
2025-05-12 08:34:43 +0900

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,3 +1,9 @@
 document.getElementById("menu-icon").addEventListener("click", function() {
-    document.getElementById("side-menu").classList.toggle("open");
+    document.getElementById("side-menu").classList.toggle("active");
+});
+
+document.addEventListener('click', function(e) {
+    if (!document.getElementById('menu-icon').contains(e.target) && !document.getElementById('side-menu').contains(e.target)) {
+        document.getElementById("side-menu").classList.remove("active");
+    }
 });
