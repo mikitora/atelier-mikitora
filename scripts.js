cb77f60d26e7b8287c94768a9c6bf4ece4bc6d66
mikitora <miki.tora.0703@gmail.com>
2025-05-12 07:23:06 +0900

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,5 +1,3 @@
-// メニュー（タブ）の開閉機能
-document.getElementById("menuButton").addEventListener("click", function() {
-  var sidebar = document.getElementById("sidebar");
-  sidebar.classList.toggle("open"); // サイドバーを表示・非表示
+document.getElementById('menuButton').addEventListener('click', function() {
+  document.getElementById('sidebar').classList.toggle('open');
 });
